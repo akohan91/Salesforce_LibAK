42cf05479f5a22842e3b3d7da8a6cd7d6f00658c
Andrei Kakhanouski <andrei.kakhanouski@ipfdigital.com>
2024-06-04 22:17:59 +0200

    Libak_0030_SobjectTable_Add_java_docs_for_the_lwc_component

--- SobjectTable/lwc/sobjectTable/sobjectTable.js
+++ SobjectTable/lwc/sobjectTable/sobjectTable.js
@@ -1,6 +1,12 @@
 /**
  * @author Andrei Kakhanouski <akohan91@gmail.com>
+ * 
+ * Lightning Web Component representing an Sobject Table.
+ * 
+ * This component provides an interface for displaying Sobject records in a table format with customizable columns
+ * and sorting options.
  */
+
 import { LightningElement, api } from 'lwc'
 import { showErrorModal } from 'c/lwcUtils'
 import { DynamicSOQLOrderBy } from 'c/soql'
@@ -8,8 +14,23 @@ import { flattenForDataTable, overrideDataTableColumns } from './sobjectTableUti
 import init from '@salesforce/apex/SobjectTableCtrl.init'
 
 export default class SobjectTable extends LightningElement {
+	/**
+	 * The name of the Apex controller that can override default (Apex implementation required).
+	 * @type {string}
+	 */
 	@api controllerName;
+
+	/**
+	 * The name of the Sobject to fetch records from.
+	 * @type {string}
+	 */
 	@api sobjectName;
+
+	/**
+	 * The fields to be selected from the Sobject.
+	 * Can be a comma-separated string or an array.
+	 * @type {(string|string[])}
+	 */
 	@api get selectFields() { return this._selectFields; }
 	set selectFields(value) {
 		try {
@@ -20,12 +41,54 @@ export default class SobjectTable extends LightningElement {
 			showErrorModal(error, this);
 		}
 	}
+
+	/**
+	 * The SOQL condition block to filter records.
+	 * @type {DynamicSOQLConditionBlock}
+	 */
 	@api conditionBlock = null;
+
+	/**
+	 * The maximum number of records to fetch.
+	 * Default is 10.
+	 * @type {number}
+	 */
 	@api limitRecords = 10;
+
+	/**
+	 * The field to sort records by.
+	 * @type {string}
+	 */
 	@api sortedBy;
+
+	/**
+	 * The direction of sorting (asc or desc).
+	 * @type {string}
+	 */
 	@api sortedDirection;
+
+	/**
+	 * The columns to override or add to the table.
+	 * {
+	 * 	action: { String}, 'ADD' | 'REMOVE' | 'OVERRIDE'
+	 * 	fieldName: { String }, name of field (column), used for 'REMOVE' and 'OVERRIDE' actions
+	 * 	column: { Object }, used for 'ADD' and 'OVERRIDE' actions
+	 * }
+	 * @type {Object[]}
+	 */
 	@api columnsToOverride;
+
+	/**
+	 * Indicates whether to hide the checkbox column for row selection.
+	 * Default is false.
+	 * @type {boolean}
+	 */
 	@api hideCheckboxColumn = false;
+
+	/**
+	 * The selected rows in the table.
+	 * @type {Object[]}
+	 */
 	@api selectedRows;
 
 	_selectFields;
@@ -56,14 +119,23 @@ export default class SobjectTable extends LightningElement {
 		}
 	}
 
+	/**
+	 * Refreshes the table by re-fetching data.
+	 */
 	@api refresh() {
 		this.initSobjectTable();
 	}
 
+	/**
+	 * Initializes the Sobject table by fetching data from the Apex controller.
+	 */
 	connectedCallback() {
 		this.initSobjectTable();
 	}
 
+	/**
+	 * Initializes the Sobject table by fetching data from the Apex controller.
+	 */
 	async initSobjectTable() {
 		try {
 			this.busy();
@@ -96,9 +168,20 @@ export default class SobjectTable extends LightningElement {
 		}
 	}
 
+	/**
+	 * Sets the isBusy flag to true to indicate that the component is busy.
+	 */
 	busy = () => (this.isBusy = true);
+
+	/**
+	 * Sets the isBusy flag to false to indicate that the component is ready.
+	 */
 	ready = () => (this.isBusy = false);
 
+	/**
+	 * Handles pagination events by updating the offset and re-fetching data.
+	 * @param {CustomEvent} event - The pagination event.
+	 */
 	handlePagination(event) {
 		try {
 			this.offsetRecords = event.detail.offset;
@@ -108,6 +191,10 @@ export default class SobjectTable extends LightningElement {
 		}
 	}
 
+	/**
+	 * Handles sorting events by updating the sortedBy and sortedDirection properties and re-fetching data.
+	 * @param {CustomEvent} event - The sort event.
+	 */
 	handleSort(event) {
 		try {
 			this.sortedBy = event.detail.columnKey;
@@ -118,6 +205,10 @@ export default class SobjectTable extends LightningElement {
 		}
 	}
 
+	/**
+	 * Handles row action events by dispatching a custom rowaction event.
+	 * @param {CustomEvent} event - The row action event.
+	 */
 	handleRowAction(event) {
 		try {
 			this.dispatchEvent(new CustomEvent('rowaction', { detail: {...event.detail} }))
@@ -126,6 +217,10 @@ export default class SobjectTable extends LightningElement {
 		}
 	}
 
+	/**
+	 * Handles row selection events by dispatching a custom rowselection event.
+	 * @param {CustomEvent} event - The row selection event.
+	 */
 	handleRowSelection(event) {
 		try {
 			this.dispatchEvent(new CustomEvent('rowselection', { detail: {...event.detail} }))
